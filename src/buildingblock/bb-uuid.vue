605b3fd0a99dd6560cc9f09d54b3cafdc376acf6
陈征 <carl.chen@chinaredstar.com>
2017-11-12 12:59:12 +0800

    把uuid的方法封装到util中

--- src/buildingblock/bb-uuid.vue
+++ src/buildingblock/bb-uuid.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import Util from '../libs/util';
+
     export default {
         name: 'bb-uuid',
         props: {
@@ -37,29 +39,7 @@
         },
         methods: {
             generate:function(){
-                var uuid = function(len, radix) {
-                  var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-                  var uuid = [], i;
-                  radix = radix || chars.length;
-                  
-                  if (len) {
-                   for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
-                  } else {
-                   var r;
-                   uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-                   uuid[14] = '4';
-                   for (i = 0; i < 36; i++) {
-                    if (!uuid[i]) {
-                     r = 0 | Math.random()*16;
-                     uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
-                    }
-                   }
-                  }
-                  
-                  return uuid.join('');
-                }
-
-                var val = uuid(this.length,this.radix);
+                var val = Util.uuid(this.length,this.radix);
                 this.v = val;
                 this.$emit('input',val);
             }
