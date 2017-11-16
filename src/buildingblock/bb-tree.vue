f06f554e878ec1d65e83fd903cbd326f6ed46338
bizhou.liu <bizhou.liu@chinaredstar.com>
2017-11-16 20:53:45 +0800

    修复bb-tree显示问题

--- src/buildingblock/bb-tree.vue
+++ src/buildingblock/bb-tree.vue
@@ -1,10 +1,11 @@
 <script>
     import Util from '../libs/util.js'
+
     export default {
         name: 'bb-tree',
         render: function (createElement) {
             const t = this;
-            if(t.canRender) {
+            if (t.canRender) {
                 let treeDom = createElement('el-tree', {
                     'class': 'bn',
                     attrs: {
@@ -19,7 +20,7 @@
 //                    },
                     props: {
                         data: t.data,
-                        props:{
+                        props: {
                             children: 'children',
                             label: t.nodeText,
                             isLeaf: 'leaf'
@@ -50,21 +51,21 @@
             value: {
                 type: String
             },
-            nodeValue:{
-                type:String,
-                default:'id'
+            nodeValue: {
+                type: String,
+                default: 'id'
             },
-            nodeText:{
-                type:String,
-                default:'name'
+            nodeText: {
+                type: String,
+                default: 'name'
             },
-            parentKey:{
-                type:String,
-                default:'parentId'
+            parentKey: {
+                type: String,
+                default: 'parentId'
             },
-            mainKey:{
-                type:String,
-                default:'id'
+            mainKey: {
+                type: String,
+                default: 'id'
             },
             multiple: {
                 type: Boolean,
@@ -73,8 +74,8 @@
             checkedField: {
                 type: Array
             },
-            opts:{
-                type:Array
+            opts: {
+                type: Array
             },
             ds: {
                 type: Object
@@ -83,36 +84,36 @@
         data() {
             return {
                 data: null,
-                canRender:false
+                canRender: false
             }
         },
         computed: {
-            randomKey(){
-                return ""+Math.floor(Math.random()*10000);
+            randomKey() {
+                return "" + Math.floor(Math.random() * 10000);
             }
         },
         created: function () {
-            const t=this;
+            const t = this;
             t.getData();
         },
-        mounted:function(){
+        mounted: function () {
         },
         methods: {
             openNode: function (node) {
                 const t = this;
-                if(node&&node[t.nodeValue]){
+                if (node && node[t.nodeValue]) {
                     t.getData(node[t.nodeValue]);
                 }
             },
-            formCommit(){
-                const t=this;
+            formCommit() {
+                const t = this;
                 let checkedNode = t.$refs.tree.getCheckedNodes();
                 let newCheckedNode = [];//所有字段
-                let checkedNodeVal =[];//只有配置的主键字段
+                let checkedNodeVal = [];//只有配置的主键字段
                 //过滤掉空字段的数据  element的bug
-                if(checkedNode&&checkedNode.length>0){
-                    checkedNode.forEach(function(item){
-                        if(item[t.nodeValue]){
+                if (checkedNode && checkedNode.length > 0) {
+                    checkedNode.forEach(function (item) {
+                        if (item[t.nodeValue]) {
                             newCheckedNode.push(item);
                             checkedNodeVal.push(item[t.nodeValue]);
                         }
@@ -121,15 +122,15 @@
                 //触发到父组件处理
                 if (!this.multiple) {
                     this.$emit("input", checkedNodeVal[0]);//让父组件能用v-model
-                }else{
+                } else {
                     this.$emit("input", checkedNodeVal.join(","));//让父组件能用v-model
                 }
                 //往上级传送选择的字段
                 this.$emit("tree-commit", newCheckedNode);
             },
             //勾选改变后
-            checkChange(data, check, childCheck){
-                const t=this;
+            checkChange(data, check, childCheck) {
+                const t = this;
                 if (check && !this.multiple) {//多选的话
                     this.$refs.tree.setCheckedKeys([]);
                     this.$refs.tree.setCheckedKeys([data[t.nodeValue]]);
@@ -138,16 +139,16 @@
             getData: function (parentId) {
                 var t = this;
                 if (t.ds) {
-                    t._parentId = parentId||0;//默认是0 ，查询根列表
-                    let inputs = t.ds.inputs;
+                    t._parentId = parentId || 0;//默认是0 ，查询根列表
+                    let inputs = t.ds.inputs || [];
                     let hasParam = false;
-                    inputs.forEach(function(item){
-                        if(item.paramName==t.parentKey){
-                            hasParam=true;
-                            item.constant=t._parentId;
+                    inputs.forEach(function (item) {
+                        if (item.paramName == t.parentKey) {
+                            hasParam = true;
+                            item.constant = t._parentId;
                         }
                     });
-                    if(!hasParam){
+                    if (!hasParam) {
                         //没有这个参数
                         inputs.push({
                             paramName: t.parentKey,
@@ -157,55 +158,55 @@
                     }
                     Util.getDSData(t.ds, {"bb": t, "router": t.$route.params}, function (map) {
                         let list = [];
-                        if(!map||map.length<=0){
+                        if (!map || map.length <= 0) {
                             return;
                         }
-                        map[0]['value'].forEach((item, key)=> {
-                            if(item['child_num']&&item['child_num']>0){
+                        map[0]['value'].forEach((item, key) => {
+                            if (item['child_num'] && item['child_num'] > 0) {
                                 //非叶子节点
-                                item.children=[{
-                                    id:''//随便加一条  能显示下拉按钮就行
+                                item.children = [{
+                                    id: ''//随便加一条  能显示下拉按钮就行
                                 }]
-                            }else{
-                                item.leaf=true;
+                            } else {
+                                item.leaf = true;
                             }
                             list.push(item);
                         });
-                        if(t._parentId&&t._parentId!=0){
+                        if (t._parentId && t._parentId != 0) {
                             //说明不是根
-                            if(t.data&&t.data.length>0){
-                                t._fillChild(t.data,t._parentId,list);
+                            if (t.data && t.data.length > 0) {
+                                t._fillChild(t.data, t._parentId, list);
                             }
-                        }else{
+                        } else {
                             //是根节点
-                            t.data= list;
+                            t.data = list;
                         }
                         t.canRender = true;
                     }, function (code, msg) {
                     });
-                } else if(t.opts&&t.opts.length>0) {
+                } else if (t.opts && t.opts.length > 0) {
                     t.data = t.opts;
                     t.canRender = true;
                 }
             },
-            _fillChild(data,parentId,list){
-                const t=this;
-                if(!data){
+            _fillChild(data, parentId, list) {
+                const t = this;
+                if (!data) {
                     return;
                 }
-                let found =false;
-                data.forEach(function(item,index){
-                    if(item[t.nodeValue]==parentId){
-                        item.children=list;
-                        found=true;
+                let found = false;
+                data.forEach(function (item, index) {
+                    if (item[t.nodeValue] == parentId) {
+                        item.children = list;
+                        found = true;
                     }
                 });
-                if(!found){
+                if (!found) {
                     //没有找到
-                    data.forEach(function(item,index){
-                        if(item.children&&item.children.length>0){
-                            let hasFound = t._fillChild(item.children,parentId,list);
-                            if(hasFound){
+                    data.forEach(function (item, index) {
+                        if (item.children && item.children.length > 0) {
+                            let hasFound = t._fillChild(item.children, parentId, list);
+                            if (hasFound) {
                                 found = true;
                                 return true;
                             }
