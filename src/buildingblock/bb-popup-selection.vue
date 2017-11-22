ddc2476c1824b4363b7b108b26f22d88fae56b7d
bizhou.liu <bizhou.liu@chinaredstar.com>
2017-11-22 14:40:28 +0800

    bb-popup-selection修复数据回填问题

--- src/buildingblock/bb-popup-selection.vue
+++ src/buildingblock/bb-popup-selection.vue
@@ -1,125 +1,136 @@
 <template>
     <div class="popup-selection">
-        <span v-if="!showSearchInput" class="showResult">{{buttonConfig.showResult?text:''}}</span>
+        <span v-if="!showSearchInput" class="showResult">{{buttonConfig.showResult ? text : ''}}</span>
         <el-button v-if="!showSearchInput" @click="showSelect" :type="buttonConfig.type" :icon="buttonConfig.icon">{{buttonName}}</el-button>
 
 
         <el-input v-if="showSearchInput" v-model="text" @keyup.native.enter="showSelect"
-                       style="width: 100%;" placeholder="请输入搜索内容" @focus="showSelect">
+                  style="width: 100%;" placeholder="请输入搜索内容" @focus="showSelect">
             <el-button type="primary" slot="append" icon="search" @click="showSelect">{{buttonName}}</el-button>
         </el-input>
         <el-dialog class="dialog" :title="title" :visible.sync="popupVisible" :modal="showModal" size="tiny">
-          <bb-list :ds="popupGrid.ds" search :popup="true" :parentParams="parentParams" :columns="popupGrid.columns" :selection="popupGrid.selection" :pagination="popupGrid.pagination" @list-select="listSelect" :searchConfig="popupGrid.searchConfig" :showHeader="popupGrid.showHeader" :highlightCurrent="highlightCurrent" :stripe="stripe" :hiddenValueKey="hiddenValueKey" :hiddenItems="hiddenItems"></bb-list>
+            <bb-list :ds="popupGrid.ds" search :popup="true" :parentParams="parentParams" :columns="popupGrid.columns"
+                     :selection="popupGrid.selection" :pagination="popupGrid.pagination" @list-select="listSelect"
+                     :searchConfig="popupGrid.searchConfig" :showHeader="popupGrid.showHeader"
+                     :highlightCurrent="highlightCurrent" :stripe="stripe" :hiddenValueKey="hiddenValueKey"
+                     :hiddenItems="hiddenItems"></bb-list>
         </el-dialog>
     </div>
 </template>
 
 <script>
     import Util from '../libs/util';
+
     export default {
         name: 'bb-popup-selection',
         props: {
-            value:{
-              type: [String,Number]
+            value: {
+                type: [String, Number]
             },
-            title:{
-              type: String,
-              default:'选择列表'
+            title: {
+                type: String,
+                default: '选择列表'
             },
-            valueField:{
+            valueField: {
                 type: String,
             },
-            valueField2:{
+            valueField2: {
                 type: String,
             },
-            textField:{
+            textField: {
                 type: String
             },
-            popupGrid:{
+            popupGrid: {
                 type: Object,
-                default:function(){
+                default: function () {
                     return {
-                        ds:null,
-                        columns:null,
-                        selection:false,
-                        pagination:false,
-                        searchConfig:null,
-                        showHeader:true //是否显示表头
+                        ds: null,
+                        columns: null,
+                        selection: false,
+                        pagination: false,
+                        searchConfig: null,
+                        showHeader: true //是否显示表头
                     }
                 }
             },
-            buttonConfig:{
+            buttonConfig: {
                 type: Object,
-                default:function(){
+                default: function () {
                     return {
-                        type:'default',//default primary text
-                        showResult:false,
-                        action:null,//execute-ds url dialog-page code buzz
-                        ds:null,
-                        goUrl:null,
-                        confirmTitle:null,
-                        confirmText:null,
-                        callback:'custom',//refresh
-                        valueKey:null,
-                        selectText:"选择",
-                        icon:null,
-                        callBackStaticWords:null
+                        type: 'default',//default primary text
+                        showResult: false,
+                        action: null,//execute-ds url dialog-page code buzz
+                        ds: null,
+                        goUrl: null,
+                        confirmTitle: null,
+                        confirmText: null,
+                        callback: 'custom',//refresh
+                        valueKey: null,
+                        selectText: "选择",
+                        icon: null,
+                        callBackStaticWords: null
                     }
                 }
             },
-            showModal:{
-                type:Boolean,
-                default:false
+            showModal: {
+                type: Boolean,
+                default: false
             },
-            highlightCurrent:{
-                type:Boolean,
-                default:true
+            highlightCurrent: {
+                type: Boolean,
+                default: true
             },
-            stripe:{
-                type:Boolean,
-                default:false
+            stripe: {
+                type: Boolean,
+                default: false
             },
-            showSearchInput:{
-                type:Boolean,
-                default:false
+            showSearchInput: {
+                type: Boolean,
+                default: false
             },
-            parentParams:{
-                type:Object
+            parentParams: {
+                type: Object
             },
-            hiddenValueKey:{
-                type:String,
+            hiddenValueKey: {
+                type: String,
             },
-            hiddenItems:{
-                type:Array,
+            hiddenItems: {
+                type: Array,
             }
         },
         data() {
             return {
-                text:this.value,
-                popupVisible:false,
-                buttonName:this.buttonConfig.selectText
+                text: this.value,
+                popupVisible: false,
+                buttonName: this.buttonConfig.selectText
             }
         },
-        created: function () {},
+        watch: {
+            value(val) {
+                this.text = val;
+            }
+        },
+        created: function () {
+        },
         methods: {
-            showSelect:function(row){
+            showSelect: function (row) {
                 this.popupVisible = true;
                 this.$emit('showPopup');
             },
-            confirmSelect:function(row){
+            confirmSelect: function (row) {
                 const t = this;
                 t.row = row;
-                if(row == 'cancelSelect'){
+                if (row == 'cancelSelect') {
                     t.popupVisible = false;
-                }else if(t.buttonConfig.action == 'execute-ds'){
+                } else if (t.buttonConfig.action == 'execute-ds') {
                     row.parentParams = t.parentParams;
-                    Util.resolveButton(t.buttonConfig,{
-                        'bb':t,
+                    Util.resolveButton(t.buttonConfig, {
+                        'bb': t,
                         "router": t.$route.params,
-                        'row-data':row,
-                        'getData':t.getData
-                    },t.afterCommit);
-                }else{
+                        'row-data': row,
+                        'getData': t.getData
+                    }, t.afterCommit);
+                } else {
                     row.parentParams = t.parentParams;
                     this.$confirm('确认选择?', '提示', {
                         confirmButtonText: '确定',
@@ -131,34 +142,34 @@
                     });
                 }
             },
-            listSelect:function(row){
+            listSelect: function (row) {
                 const t = this;
-                if(typeof row != 'array' && row.length !=0){
+                if (typeof row != 'array' && row.length != 0) {
                     t.confirmSelect(row)
                 }
             },
-            afterCommit:function(button){
+            afterCommit: function (button) {
                 const t = this;
                 t.popupVisible = false;
-                if(button['refreshParent'] && t.parentParams){
+                if (button['refreshParent'] && t.parentParams) {
                     t.parentParams.getData();
                 }
-                if(button.goUrl){
+                if (button.goUrl) {
                     t.$router.push(button.goUrl);
                 }
                 t.outPutValue();
-                t.$emit('afterCommit',t);
+                t.$emit('afterCommit', t);
             },
-            outPutValue:function(){
+            outPutValue: function () {
                 const t = this;
                 const row = t.row
                 t.popupVisible = false;
                 t.text = row[t.textField];
-                if(t.buttonConfig.showResult){
+                if (t.buttonConfig.showResult) {
                     t.buttonName = '修改';
                     t.buttonConfig.type = 'text';
                 }
-                const data = row[t.valueField2]?row[t.valueField2]:row[t.valueField];//兼容接口选择列表的alias 和 url  支持多参数输出
+                const data = row[t.valueField2] ? row[t.valueField2] : row[t.valueField];//兼容接口选择列表的alias 和 url  支持多参数输出
                 t.$emit('input', data);
                 t.$emit('change', row);
             }
@@ -166,20 +177,22 @@
     }
 </script>
 <style lang="less">
-    .popup-selection{
+    .popup-selection {
         //max-width: 420px;
         display: inline-block;
         .el-dialog__body {
             padding: 10px 20px;
         }
     }
-    .popup-selection .current-row>td{
+
+    .popup-selection .current-row > td {
         background-color: #edf7ff !important;
         font-family: 'PingFangSC-Regular';
         font-size: 14px;
         color: #0091EA;
     }
-    .popup-selection .dialog>div{
+
+    .popup-selection .dialog > div {
         min-width: 500px;
     }
 </style>
