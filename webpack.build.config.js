aac4f3f40063df56d8eb23558ea5143bd0218e16
陈征 <carl.chen@chinaredstar.com>
2017-11-22 09:50:55 +0800

    打包脚本

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,12 +1,17 @@
+const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
-const webpackBaseConfig = require('./webpack.base.config.js');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var env = process.env.ENV;
 
-module.exports = merge(webpackBaseConfig, {
+module.exports = {
+    entry: {
+        // main: './src/index'
+        main: './src/buildingblock/bb-tag'
+    },
     output: {
+        path: path.join(__dirname, './dist'),
         publicPath: '/',
         filename: '[name].js',
         chunkFilename: '[name].chunk.js'
@@ -26,5 +31,76 @@ module.exports = merge(webpackBaseConfig, {
         //         warnings: false
         //     }
         // })
-    ]
-});
+    ],
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+
+                        less: ExtractTextPlugin.extract({
+                            use: ['css-loader?minimize', 'autoprefixer-loader', 'less-loader'],
+                            fallback: 'vue-style-loader'
+                        }),
+
+                        sass: ExtractTextPlugin.extract({
+                            use: ['css-loader?minimize', 'autoprefixer-loader', 'sass-loader'],
+                            fallback: 'vue-style-loader'
+                        }),
+
+                        css: ExtractTextPlugin.extract({
+                            use: ['css-loader', 'autoprefixer-loader'],
+                            fallback: 'vue-style-loader'
+                        })
+                    }
+                }
+            }, {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                exclude: /node_modules/
+            }, {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader?minimize', 'autoprefixer-loader'],
+                    fallback: 'style-loader'
+                })
+            },
+
+            {
+                test: /\.less/,
+                use: ExtractTextPlugin.extract({
+                    use: ['autoprefixer-loader', 'less-loader'],
+                    fallback: 'style-loader'
+                })
+            },
+
+            {
+                test: /\.sass/,
+                use: ExtractTextPlugin.extract({
+                    use: ['autoprefixer-loader', 'sass-loader'],
+                    fallback: 'style-loader'
+                })
+            },
+
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=1024'
+            }, {
+                test: /\.(html|tpl)$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            'vue': 'vue/dist/vue.esm.js',
+            jQuery: 'jquery',//解决art-dialog打包报错的问题
+            jquery: 'jquery',
+        }
+    }
+};
