b3c96eaa2130437657d6342ea7685d4d1728b19f
minjie.wu <minjie.wu@chinaredstar.com>
2017-12-15 20:32:43 +0800

    新增bb-pop-select

---
+++ src/buildingblock/bb-pop-select.vue
@@ -0,0 +1,26 @@
+<template>
+</template>
+
+<script>
+    export default {
+        name: 'bb-pop-select',
+        props: {
+            value:{
+                type:[String,Object]
+            }
+        },
+
+        data() {
+            return {
+            }
+        },
+        watch: {
+        },
+        created: function () {
+        },
+        mounted:function(){
+        },
+        methods: {
+        }
+    }
+</script>
