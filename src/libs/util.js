17f2b7735b3255303af338668f8f3901b2482b5e
minjie.wu <minjie.wu@chinaredstar.com>
2017-12-18 11:43:02 +0800

    1.删除t  在eval时不必传参数也能拿到外部所有参数

--- src/libs/util.js
+++ src/libs/util.js
@@ -244,7 +244,7 @@ util.resolveButton = function(button, valueobj) {
                 });
                 // util.buttonCallback(button, valueobj, callback, err);
                 //触发按钮执行完成事件
-                t.$emit("button-finish",button,valueobj,map);
+                t.$emit("button-finish",button,valueobj,err);
             });
         }).catch(() => {
             t.$message({
@@ -277,7 +277,7 @@ util.resolveButton = function(button, valueobj) {
         //如果是巴斯代码，远程加载
         util.loadBuzz(button.buzz, function(code) {
             t.util = util;
-            eval(code)(t);
+            eval(code);
             //触发按钮执行完成事件
             t.$emit("button-finish",button,valueobj);
         });
