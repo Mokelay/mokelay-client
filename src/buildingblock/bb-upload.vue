10a31014d1e693d553fd4f7e95056f415dc71dd6
minjie.wu <minjie.wu@chinaredstar.com>
2017-12-19 19:12:05 +0800

    优化bb-upload  支持handle 处理数据

--- src/buildingblock/bb-upload.vue
+++ src/buildingblock/bb-upload.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
         <el-upload
-            action="http://hxwy.dev.rs.com/wy/uploadMany"
-            list-type="picture-card"
-            :on-preview="handlePictureCardPreview"
-            :on-remove="handleRemove">
+            :action="uploadUrl"
+            :on-success="onSuccess"
+            :on-error="onError"
+            :on-preview="picturePreview"
+            :on-remove="handleRemove"
+            :list-type="listType"
+            :limit="limit"
+            :accept="fileType"
+            >
             <i class="el-icon-plus"></i>
         </el-upload>
         <el-dialog :visible.sync="dialogVisible" size="tiny">
@@ -12,63 +17,78 @@
         </el-dialog>
     </div>
 </template>
-
 <script>
     export default {
         name: 'bb-upload',
         props: {
+            //上传接口配置
             uploadDs:{
                 type:Object
             },
-            deleteDs:{
-                type:Object
+            //上传组件展示类型  text：只显示文件名称   picture：简单图片   picture-card：卡片
+            listType:{
+                type:String,
+                default:'picture-card'
+            },
+            //限制上传数量
+            limit:{
+                type:Number
+            },
+            //文件类型
+            fileType:{
+                type:String,
+                default:'image/png,image/gif,image/jpeg'
+            },
+            //处理图片地址数组
+            handle:{
+                type:String
             }
         },
         data() {
             return {
                 dialogImageUrl: '',
-                dialogVisible: false
+                dialogVisible: false,
+                uploadUrl:''
             }
         },
         watch: {
         },
         created: function () {
+            if(this.uploadDs){
+                this.uploadUrl = this.uploadDs.api
+            }
+            
         },
         mounted:function(){
         },
         methods: {
-            uploadFile:function(val){
-                const t = this;
-                if (t.uploadDs) {
-                    _TY_Tool.getDSData(t.uploadDs, {"bb": t, "router": t.$route.params}, function (map) {
-                        map.forEach((val,key)=>{
-                           const dataKey = val.dataKey
-                            t[dataKey] = val.value.list;
-                        })
-                        t.loadCahrts()
-                    }, function (code, msg) {
-                    });
-                }
-            },
-            deleteFile:function(){
-                const t = this;
-                if (t.deleteDs) {
-                    _TY_Tool.getDSData(t.deleteDs, {"bb": t, "router": t.$route.params}, function (map) {
-                        map.forEach((val,key)=>{
-                           const dataKey = val.dataKey
-                            t[dataKey] = val.value.list;
-                        })
-                        t.loadCahrts()
-                    }, function (code, msg) {
-                    });
-                }
-            },
+            //移除图片
             handleRemove:function(file, fileList) {
-                console.log(file, fileList);
+                const t = this;
+                t.handleFileList('remove',fileList);
             },
-            handlePictureCardPreview:function(file) {
+            //预览图片
+            picturePreview:function(file) {
                 this.dialogImageUrl = file.url;
                 this.dialogVisible = true;
+            },
+            //上传成功
+            onSuccess:function(response,file,fileList){
+                const t = this;
+                t.handleFileList('success',fileList)
+            },
+            //上传失败
+            onError:function(err,file,fileList){
+                const t = this;
+                t.handleFileList('err',fileList)
+            },
+            handleFileList:function(emit,fileList){
+                const t = this;
+                _TY_Tool.loadBuzz(t.handle, function(code) {
+                    fileList = eval(code);
+                    t.$emit(emit,fileList);
+                    t.$emit('input',fileList);
+                });
             }
         }
     }
