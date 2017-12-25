2e0c3cdb2ca47301f507094edc3b3d15fde3ef1d
minjie.wu <minjie.wu@chinaredstar.com>
2017-12-25 15:38:20 +0800

    bb-deta-picker支持handle

--- src/buildingblock/bb-date-picker.vue
+++ src/buildingblock/bb-date-picker.vue
@@ -3,7 +3,7 @@
         v-model="valueBase"
         :disabled="option.disabled"
         :editable="option.editable"
-        :picker-options="option.pickerOptions"
+        :picker-options="pickerOptions"
         :value-format="option.valueFormat"
         @change="change"
         :type="type" placeholder="选择日期时间"></el-date-picker>
@@ -17,12 +17,13 @@
         props: {
             option:{
                 type: Object,
-                default:function(){
-                    return {
+                /*数据格式
+                    {
                         disabled:false,
-                        editable:false
+                        editable:false,
+                        handle:buzz //处理option 
                     }
-                }
+                */
             },
             value:{
                 type:[String,Date]
@@ -37,7 +38,10 @@
         },
         data() {
             return {
+                //默认值 
                 valueBase: this.value,
+                //日期选择器参数配置
+                pickerOptions:null
             }
         },
         watch:{
@@ -46,6 +50,8 @@
             }
         },
         created: function () {
+            const t = this;
+            t.getPickerOptions();
         },
         mounted:function(){
             let t=this;
@@ -55,6 +61,19 @@
             change:function(val){
                 this.$emit('input',val)
                 this.$emit('change',val)
+            },
+            //通过buzz处理PickerOptions，实现返回自定义函数
+            getPickerOptions:function(){
+                const t = this;
+                if(t.option.handle){
+                    //日期选择器参数 通过buzz返回配置
+                    _TY_Tool.loadBuzz(t.option.handle, function(code) {
+                        t.pickerOptions = eval(code);
+                    });
+                }else{
+                    //日期选择器参数配置
+                    t.pickerOptions = Object.assign({},t.option);
+                }
             }
         }
     }
