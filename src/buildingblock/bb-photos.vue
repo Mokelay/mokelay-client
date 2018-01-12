abfd3010a440bac668181a33d083f3ab001e48f7
minjie.wu <minjie.wu@chinaredstar.com>
2018-01-12 16:14:44 +0800

    bb-photos 支持动态数据源，支持手动切换

--- src/buildingblock/bb-photos.vue
+++ src/buildingblock/bb-photos.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="bb-photos" @touchend="swipe">
+    <div class="bb-photos" v-swipeleft="swipe" v-swiperight="swipe">
+        
         <ul>
             <li v-for="(pic,index) in realFields" :key="index">
                 <transition 
                     name="custom-classes-transition"
                     :enter-active-class="transformAnimate.enter"
                     :leave-active-class="transformAnimate.leave"
+                    @after-leave="afterLeave"
                 >
-                    <img v-if="pic.show":src="pic.url" :alt="pic.name">
+                    <img v-show="pic.show":src="pic.url" :alt="pic.name">
                 </transition>
             </li>
         </ul>
@@ -24,7 +26,7 @@
                 字符串：'https://a.jpg,https://b.jpg'
             */
             fields:{
-                type:[Array,String]
+                type:[Array,String],
             },
             /*  fieldsDs 动态图片数据源*/
             fieldsDs:{
@@ -54,12 +56,14 @@
             return {
                 show:true,
                 realFields:this.fields,
-                showArr:[],
-                int:0,
                 transformAnimate:{
                     enter:'',
                     leave:''
-                }
+                },
+                goNext:true,
+                lastItem:null,
+                nowItem:0,
+                nextItem:null,
             }
         },
         computed:{
@@ -69,63 +73,133 @@
         created: function () {
         },
         mounted:function(){
-             this.setTransformAnimate();
-             this.setShow();
-             if(this.transformConfig.transformMode == 'auto'){
-                this.autoTransform();
-             };
+            this.getData();
+            this.setTransformAnimate();
+            if(this.realFields){
+                this.setShow();
+            }
         },
         methods: {
-            //设置图片显示
+            //获取图片地址
+            getData: function () {
+                const t = this;
+                if (t.fieldsDs) {
+                    _TY_Tool.getDSData(t.fieldsDs, {"bb": t, "router": t.$route.params,"external":t.external}, function (map) {
+                        map.forEach((val,key)=>{
+                            const dataKey = val.dataKey
+                            t.realFields = val.value.list;
+                            //触发积木更新
+                            if(t.realFields){
+                                t.setShow();
+                            }
+                        })
+                    }, function (code, msg) {
+                    });
+                }
+            },
+            //支持外部传参
+            linkage:function(...data){
+                if(data){
+                    this.external['linkage'] = data;
+                    this.getData();
+                }
+            },
+            //初始化图片显示
             setShow:function(){
                 const t = this;
-                t.showArr = [];
                 t.realFields.forEach((val,key) => {
                     val.show = false;
                 });
+                //触发自动轮播
+                t.autoTransform();
+                //触发积木状态更新
                 const ele = t.realFields[0];
                 ele.show = true;
-                t.$set(t.realFields,0,ele)
+                t.$set(t.realFields,0,ele);
             },
             //设置自动轮播
             autoTransform:function(){
                 const t = this;
-                let int = 0;
-                setInterval(function(){
-                    t.realFields[int].show = true;
-                    //隐藏上一个
-                    let lastItem = 0;
-                    if(int == 0){
-                        lastItem = t.realFields.length - 1
-                    }else{
-                        lastItem = int - 1
-                    }
-                    t.realFields[lastItem].show = false;
-                    //当指针大于数组长度重新循环
-                    if(int == t.realFields.length - 1){
-                        int = 0;
-                    }else{
-                        int++; 
-                    }
-                    const ele = t.realFields[int]
-                    t.$set(t.realFields,int,ele)
-                },t.transformConfig.transformTime);
+                if(t.transformConfig.transformMode == 'auto'){
+                    t.setTransformAnimate();
+                    t.setInterval = setInterval(t.nextPhoto,t.transformConfig.transformTime);
+                }
             },
-            //获取切换动画
+            //计算并下一张动画
+            nextPhoto:function(){
+                
+                const t = this;
+                if(t.goNext){
+                    //前进
+                    let nextItem = t.nowItem == t.realFields.length - 1?0:t.nowItem+1;
+                    t.realFields[t.nowItem].show = false;
+                    t.realFields[nextItem].show = true;
+                    t.nowItem = nextItem
+                }else{
+                    console.log('in fan');
+                    //后退
+                    let nextItem = t.nowItem == 0?t.realFields.length - 1:t.nowItem-1;
+                    t.realFields[t.nowItem].show = false;
+                    t.realFields[nextItem].show = true;
+                    t.nowItem = nextItem
+                }
+                const ele = t.realFields[t.nowItem];
+                t.$set(t.realFields,t.nowItem,ele);
+            },
+            //初始化切换动画
             setTransformAnimate:function(){
                 const t = this;
-                t.transformAnimate.enter ='animated ' + t.transformConfig.transformAnimate.split(',')[0];
-                t.transformAnimate.leave ='animated ' + t.transformConfig.transformAnimate.split(',')[1];
+                const animate = t.transformConfig.transformAnimate.split(',');
+                if(t.goNext){
+                    //正向入场动画
+                    t.transformAnimate.enter ='animated ' + animate[0];
+                    //正向离场动画
+                    t.transformAnimate.leave ='animated ' + animate[1];  
+                }else{
+                    //反向入场动画
+                    t.transformAnimate.enter ='animated ' + animate[2];
+                    //反向离场动画
+                    t.transformAnimate.leave ='animated ' + animate[3];
+                }
             },
-            swipe:function(params){
-
+            //滑动触发图片转换
+            swipe:function(s,e){
+                const t = this;
+                if(s.deltaX > 100){
+                    t.goNext = false;
+                    clearInterval(t.setInterval);
+                    //计算并下一张动画
+                    t.nextPhoto();
+                    //修改动画
+                    t.setTransformAnimate();
+                }
+                if(s.deltaX < -100){
+                    t.goNext = true;
+                    clearInterval(t.setInterval);
+                    t.nextPhoto();
+                    t.autoTransform();
+                }  
+            },
+            //出动画之后纠正轮播状态为向前
+            afterLeave:function(){
+                const t = this;
+                if(!t.goNext){
+                    t.goNext = true;
+                    //还原正向动画
+                    t.setTransformAnimate();
+                    t.autoTransform();
+                }
             }
         }
     }
 </script>
-<style lang='less' scoped>
+<style lang='less'>
     .bb-photos{
         width: 100%;
+        min-height: 300px;
+        ul{
+            width:100%;
+        }
         ul li{
             position: absolute;
             top: 0;
