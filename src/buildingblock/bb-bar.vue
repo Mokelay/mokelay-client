d5b53575b87c51a7da1be9e99fa994b741e79bcd
minjie.wu <minjie.wu@chinaredstar.com>
2018-01-25 18:58:01 +0800

    bb-bar优化基础样式实现基础功能

--- src/buildingblock/bb-bar.vue
+++ src/buildingblock/bb-bar.vue
@@ -1,8 +1,17 @@
 <template>
-    <div class="bb-bar">
-        <bb-read-bb></bb-read-bb>
-        <bb-read-bb></bb-read-bb>
-        <bb-read-bb></bb-read-bb>
+    <div class="bb-bar" :style="{'background-image':realBasicConfig.backgroundColor,color:realBasicConfig.fontColor,height:realBasicConfig.height,'line-height':realBasicConfig.height}">
+        <div class="bb-bar-left">
+            <bb-button :button="logoButton" :style="{height:'100%',width:realBasicConfig.logoWidth}" class="bb-bar-logo">
+                <img v-if="!logoButton.icon" :src="realBasicConfig.logoUrl" alt=""> 
+            </bb-button>   
+            <bb-read-bb class="bb-list" :fields="realFieldsLeft"></bb-read-bb>
+        </div>
+        <div class="bb-bar-middle">
+            <bb-read-bb class="bb-list" :fields="realFieldsMiddle"></bb-read-bb>
+        </div>
+        <div class="bb-bar-right">
+            <bb-read-bb class="bb-list" :fields="realFieldsRight"></bb-read-bb>
+        </div>
     </div>
 </template>
 
@@ -13,23 +22,35 @@
             /*
                 basicConfig 样式配置
                 {
-                    logoUrl:'logo图片地址',
+                    logoUrl:'logo图片地址',  logo的图表的和图片互斥 都配置时 优先显示图标
                     logoHref:'点击跳转地址'
                     backgroundColor:'':'', 背景色
-                    imgWidth:'', 宽度
-                    imgHeight:'', 高度
+                    height:'', bar高度
+                    logoWidth:'', logo宽度
                     fontColor:'', 标题字体颜色
                     fontSize:'',    标题字体大小
                 }
             */
             basicConfig:{
-                type:Object
+                type:Object,
+                default:function(){
+                    return{
+                        logoIcon:'ty-icon_M_b ty-font',
+                        logoUrl:'',
+                        logoHref:'',
+                        backgroundColor:'-webkit-linear-gradient(0deg, #1E90FF,#104E8B)',
+                        logoWidth:'120px',
+                        height:'50px',
+                        fontColor:'#ffffff',
+                        fontSize:'30px',
+                    }
+                }
             },
             /*
                 bbFieldsLeft 左侧积木
                 [{
-                    bb:'bb-button' || 'bb-dropdown'
-                    bbProps:{ //bb:logo
+                    et:'bb-button' || 'bb-dropdown'
+                    etProps:{ //bb:logo
                         url:'logo图片地址',
                         href:'点击跳转地址'
                     }
@@ -41,8 +62,8 @@
             /*
                 bbFieldsMiddle 中间积木
                 [{
-                    bb:'bb-button' || 'bb-dropdown'
-                    bbProps:{}
+                    et:'bb-button' || 'bb-dropdown'
+                    etProps:{}
                 }]
             */
             bbFieldsMiddle:{
@@ -51,8 +72,8 @@
             /*
                 bbFieldsRight 右侧积木
                 [{
-                    bb:'bb-button' || 'bb-dropdown'
-                    bbProps:{}
+                    et:'bb-button' || 'bb-dropdown'
+                    etProps:{}
                 }]
             */
             bbFieldsRight:{
@@ -62,6 +83,19 @@
         },
         data() {
             return {
+                realBasicConfig:this.basicConfig,
+                realFieldsLeft:this.bbFieldsLeft,
+                realFieldsMiddle:this.bbFieldsMiddle,
+                realFieldsRight:this.bbFieldsRight,
+                //logo 点击后的跳转地址
+                logoButton:{
+                    type:'text',
+                    action:'url',
+                    url:this.basicConfig.logoHref,
+                    icon:this.basicConfig.logoIcon,
+                    color:this.basicConfig.fontColor,
+                    fontSize:this.basicConfig.fontSize,
+                }
             }
         },
         watch: {
@@ -74,6 +108,58 @@
         }
     }
 </script>
-<style lang='less' scoped>
-
+<style lang='less'>
+    .bb-bar{
+        height: 50px;
+        display: flex;
+        &>div{
+            display: inline-block;
+            flex: 1;
+        }
+        .bb-bar-left{
+            text-align: left;
+            .bb-list{
+                justify-content: flex-end;
+            }
+            &>div,button{
+                display: inline-block;
+                float: left;
+            }
+            .bb-bar-logo{
+                display: inline-block;
+                float: left;
+                margin:0 10px 0 0;
+                padding: 0;
+                border: none;
+                img{
+                    height: 100%;
+                }
+            }
+        }
+        .bb-list{
+            cursor: pointer;
+            display: flex;
+            position: relative;
+            height: 100%;
+            align-items: center;
+            justify-content: center;
+            &>div{
+                margin: 0 5px;
+            }
+            &>button{
+                margin: 0 5px;
+            }
+        }
+        .bb-bar-middle{
+            justify-content: center;
+        }
+        .bb-bar-right{
+            padding-right: 10px;
+            text-align: right;
+            .bb-list{
+                justify-content: flex-end;
+            }
+            
+        }
+    }
 </style>
