1634380b8a0b0eea7726b963685e191bf1f31cb4
minjie.wu <minjie.wu@chinaredstar.com>
2018-02-27 16:33:47 +0800

    1.实现8种布局 2.通过bb-layout-seriation渲染标准content数据

--- src/layout/bb-container.vue
+++ src/layout/bb-container.vue
@@ -1,70 +1,114 @@
 <template>
-    <div class="bb-container">
+    <div class="bb-container template">
         <!-- 上下布局 -->
         <el-container v-if="realLayout == 'layout1'">
             <el-header>
-                <bb-read-bb :fields="realHeader"></bb-read-bb>
+                <bb-layout-seriation :content="realHeader" :horizontal="true"></bb-layout-seriation>
             </el-header>
             <el-main>
-                <bb-read-bb :fields="realMain"></bb-read-bb>
+                <bb-layout-seriation :content="realMain"></bb-layout-seriation>
             </el-main>
         </el-container>
         <!-- 上中下布局 -->
         <el-container v-if="realLayout == 'layout2'">
-            <el-header>Header</el-header>
-            <el-main>Main</el-main>
-            <el-footer>Footer</el-footer>
+            <el-header>
+                <bb-layout-seriation :content="realHeader" :horizontal="true"></bb-layout-seriation>
+            </el-header>
+            <el-main>
+                <bb-layout-seriation :content="realMain"></bb-layout-seriation>
+            </el-main>
+            <el-footer>
+                <bb-layout-seriation :content="realFooter" :horizontal="true"></bb-layout-seriation>
+            </el-footer>
         </el-container>
         <!-- 左右布局 -->
         <el-container v-if="realLayout == 'layout3'">
-            <el-aside width="200px">Aside</el-aside>
-            <el-main>Main</el-main>
+            <el-aside width="20%">
+                <bb-layout-seriation :content="realLeftAside"></bb-layout-seriation>
+            </el-aside>
+            <el-main>
+                <bb-layout-seriation :content="realMain"></bb-layout-seriation>
+            </el-main>
         </el-container>
         <!-- 上下左右布局 -->
         <el-container v-if="realLayout == 'layout4'">
-            <el-header>Header</el-header>
+            <el-header>
+                <bb-layout-seriation :content="realHeader" :horizontal="true"></bb-layout-seriation>
+            </el-header>
             <el-container>
-                <el-aside width="200px">Aside</el-aside>
-                <el-main>Main</el-main>
+                <el-aside width="20%">
+                    <bb-layout-seriation :content="realLeftAside"></bb-layout-seriation>
+                </el-aside>
+                <el-main>
+                    <bb-layout-seriation :content="realMain"></bb-layout-seriation>
+                </el-main>
             </el-container>
         </el-container>
         <!-- 上下左右上下布局 -->
         <el-container v-if="realLayout == 'layout5'">
-            <el-header>Header</el-header>
+            <el-header>
+                <bb-layout-seriation :content="realHeader" :horizontal="true"></bb-layout-seriation>
+            </el-header>
             <el-container>
-                <el-aside width="200px">Aside</el-aside>
+                <el-aside width="20%">
+                    <bb-layout-seriation :content="realLeftAside"></bb-layout-seriation>
+                </el-aside>
                 <el-container>
-                    <el-main>Main</el-main>
-                    <el-footer>Footer</el-footer>
+                    <el-main>
+                        <bb-layout-seriation :content="realMain"></bb-layout-seriation>
+                    </el-main>
+                    <el-footer>
+                        <bb-layout-seriation :content="realFooter" :horizontal="true"></bb-layout-seriation>
+                    </el-footer>
                 </el-container>
             </el-container>
         </el-container>
         <!-- 左右上下布局 -->
         <el-container v-if="realLayout == 'layout6'">
-            <el-aside width="200px">Aside</el-aside>
+            <el-aside width="20%">
+                <bb-layout-seriation :content="realLeftAside"></bb-layout-seriation>
+            </el-aside>
             <el-container>
-                <el-header>Header</el-header>
-                <el-main>Main</el-main>
+                <el-header>
+                    <bb-layout-seriation :content="realHeader" :horizontal="true"></bb-layout-seriation>
+                </el-header>
+                <el-main>
+                    <bb-layout-seriation :content="realMain"></bb-layout-seriation>
+                </el-main>
             </el-container>
         </el-container>
         <!-- 左右上中下布局 -->
         <el-container v-if="realLayout == 'layout7'">
-            <el-aside width="200px">Aside</el-aside>
+            <el-aside width="20%">
+                <bb-layout-seriation :content="realLeftAside"></bb-layout-seriation>
+            </el-aside>
             <el-container>
-                <el-header>Header</el-header>
-                <el-main>Main</el-main>
-                <el-footer>Footer</el-footer>
+                <el-header>
+                    <bb-layout-seriation :content="realHeader" :horizontal="true"></bb-layout-seriation>
+                </el-header>
+                <el-main>
+                    <bb-layout-seriation :content="realMain"></bb-layout-seriation>
+                </el-main>
+                <el-footer>
+                    <bb-layout-seriation :content="realFooter" :horizontal="true"></bb-layout-seriation>
+                </el-footer>
             </el-container>
         </el-container>
         <!-- 上下左中右布局 -->
         <el-container v-if="realLayout == 'layout8'">
-            <el-header>Header</el-header>
+            <el-header>
+                <bb-layout-seriation :content="realHeader" :horizontal="true"></bb-layout-seriation>
+            </el-header>
             <el-container>
-                <el-aside width="200px">leftAside</el-aside>
+                <el-aside width="20%">
+                    <bb-layout-seriation :content="realLeftAside"></bb-layout-seriation>
+                </el-aside>
                 <el-main>
-                    <bb-read-bb :fields="realMain.content"></bb-read-bb>
+                    <bb-layout-seriation :content="realMain"></bb-layout-seriation>
                 </el-main>
-                <el-aside width="200px">rightAside</el-aside>
+                <el-aside width="20%">
+                    <bb-layout-seriation :content="realRightAside"></bb-layout-seriation>
+                </el-aside>
             </el-container>
         </el-container>
     </div> 
@@ -91,43 +135,88 @@
             },
             /*
                 header leftAside rightAside main footer 头部积木静态数据配置
-                {
-                    content:[{uuid:'',alias:'',aliasName:'',attributes:{},layout:""}],
-                    interactives:[{uuid:'',fromContentUUID:'',fromContentEvent:'',executeContentUUID:'',executeContentMethodName:''}]
-                }
+                    //积木数组
+                    content:[{                      //页面内容
+                        uuid:'',
+                        alias:'',                   //积木别名
+                        aliasName:'',               //中文名称
+                        attributes:{},              //积木属性
+                        animation:[{                //动画
+                            style:"",               //方式
+                            time:0,                 //时间
+                            delay:0,                //延迟时间
+                            playNum:1               //播放次数
+                            loop:true|false,        //循环
+                            direction:""            //方向
+                        }],
+                        interactives:[{             //触发交互
+                            uuid:'',
+                            fromContentEvent:'',    //触发积木的事件,fromContentUUID为当前content的UUID
+                            executeType:'',         //执行类型(预定义方法 trigger_method,
+                                                    //自定义方法 custom_script,
+                                                    //容器类方法 container_method)
+                            executeScript:'',       //执行脚本 executeType = custom_script
+                            executeContentUUID:'',  //执行积木的UUID executeType = trigger_method
+                            executeContentMethodName:'',
+                                                    //执行积木的方法
+                            containerMethodName:''  //容器方法 executeType = container_method
+                        }],
+                        layout:{                    //积木布局
+                            sort:0,                 //积木排序
+                            bgColor:"",             //背景颜色
+                            rotate:0,               //旋转
+                            transparency:0,         //透明度
+                            size:{width:0,height:0},//大小
+                            position:{x:0,y:0},     //位置
+                            border:{                //边框
+                                style:"",           //边框样式
+                                color:"",           //边框颜色
+                                size:"",            //边框尺寸
+                                radian:"",          //边框弧度
+                                margin:""           //边距
+                            },
+                            shadow:{                //阴影
+                                color:"",           //阴影颜色
+                                size:"",            //阴影大小
+                                direction:'',       //阴影方向
+                                vague:''            //阴影模糊
+                            }
+                        }
+                    }]
             */
             header:{
-                type:Object
-            },
-            /*
-                leftAside 侧边栏积木配置
-            */
-            leftAside:{
-                type:Object
-            },
-            /*
-                rightAside 侧边栏积木配置
-            */
-            rightAside:{
-                type:Object
-            },
-            /*
-                main 头部积木配置
-            */
-            main:{
-                type:Object,
+                type:Array,
                 default:function(){
-                    return {
-                        content:[{uuid:'1234',alias:'bb-input',aliasName:'',attributes:{},layout:""}],
-                        interactives:[{uuid:'',fromContentUUID:'',fromContentEvent:'',executeContentUUID:'',executeContentMethodName:''}]
-                    }
+                    return[{uuid:'header',alias:'bb-words',aliasName:'',attributes:{value:'Header',textAlign:'center',lineHeight:'60px'},animation:[],interactives:[]}]
                 }
             },
-            /*
-                footer 底部积木配置
-            */
+            /*leftAside 侧边栏积木配置*/
+            leftAside:{
+                type:Array,
+                default:function(){
+                    return[{uuid:'leftAside',alias:'bb-words',aliasName:'',attributes:{value:'LeftAside',textAlign:'center',lineHeight:'60px'},animation:[],interactives:[]}]
+                }
+            },
+            /*rightAside 侧边栏积木配置*/
+            rightAside:{
+                type:Array,
+                default:function(){
+                    return[{uuid:'rightAside',alias:'bb-words',aliasName:'',attributes:{value:'RightAside',textAlign:'center',lineHeight:'60px'},animation:[],interactives:[]}]
+                }
+            },
+            /*main 头部积木配置*/
+            main:{
+                type:Array,
+                default:function(){
+                    return[{uuid:'main',alias:'bb-words',aliasName:'',attributes:{value:'Main',textAlign:'center',lineHeight:'60px'},animation:[],interactives:[]}]
+                }
+            },
+            /*footer 底部积木配置*/
             footer:{
-                type:Object
+                type:Array,
+                default:function(){
+                    return[{uuid:'footer',alias:'bb-words',aliasName:'',attributes:{value:'Footer',textAlign:'center',lineHeight:'60px'},animation:[],interactives:[]}]
+                }
             },
             /*
                 containerDs 动态数据源配置 
@@ -163,9 +252,12 @@
                 if (t.containerDs) {
                     _TY_Tool.getDSData(t.containerDs, _TY_Tool.buildTplParams(t), function (map) {
                         map.forEach((val,key)=>{
-                            const dataKey = val.dataKey
-                            t.realFields = t.realFields.concat(val.value);
-                            t.setRealFields();
+                            t.realLayout = val.value.layout;
+                            t.realHeader = val.value.header;
+                            t.realLeftAside = val.value.leftAside;
+                            t.realRightAside = val.value.rightAside;
+                            t.realMain = val.value.main;
+                            t.realFooter = val.value.footer;
                         })
                     }, function (code, msg) {
                     });
@@ -175,37 +267,16 @@
     }
 </script>
 <style lang='less' scoped>
-    .el-header, .el-footer {
-        background-color: #B3C0D1;
-        color: #333;
-        text-align: center;
-        line-height: 60px;
-    }
-
-    .el-aside {
-        background-color: #D3DCE6;
-        color: #333;
-        text-align: center;
-        line-height: 200px;
-    }
-
-    .el-main {
-        background-color: #E9EEF3;
-        color: #333;
-        text-align: center;
-        line-height: 160px;
-    }
-
-    .bb-container > .el-container {
-        margin-bottom: 40px;
-    }
-
-    .el-container:nth-child(5) .el-aside,
-    .el-container:nth-child(6) .el-aside {
-        line-height: 260px;
-    }
-
-    .el-container:nth-child(7) .el-aside {
-        line-height: 320px;
+    .template{
+        .el-header, .el-footer {
+            background-color: #B3C0D1;
+        }
+        .el-aside {
+            min-width:200px;
+            background-color: #D3DCE6;
+        }
+        .el-main {
+            background-color: #E9EEF3;
+        }
     }
 </style>
