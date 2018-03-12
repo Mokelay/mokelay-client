e71fb95f4799dea735cf2ba4014cf5f40bf82c4e
minjie.wu <minjie.wu@chinaredstar.com>
2018-03-12 15:07:09 +0800

    还原积木linkage方法，优化交互方法传参，兼容原数据格式

--- src/buildingblock/bb-cascader.vue
+++ src/buildingblock/bb-cascader.vue
@@ -149,7 +149,7 @@
             linkage(data){
               let t=this;
               if(data){
-                this.external['linkage'] = data[0];
+                this.external['linkage'] = data;
                 //刷新选项
                 t.getNextData(1);
               }
