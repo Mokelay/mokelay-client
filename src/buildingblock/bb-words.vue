8dea05102e7657c78cc53dd5bfa84b5aabaf60d0
minjie.wu <minjie.wu@chinaredstar.com>
2018-03-19 15:02:17 +0800

    修复重复渲染问题

--- src/buildingblock/bb-words.vue
+++ src/buildingblock/bb-words.vue
@@ -193,7 +193,6 @@
         t.external['linkage'] = data;
         if(data){
           t.realText = data;
-          t.tansferTpl();
         }
       },
        //外部联动 接收交互参数  上面方法的重载,如果有多个参数会走下面这个方法
@@ -202,7 +201,6 @@
         t.external['linkage'] = data;
         if(data[0]){
           t.realText = data[0];
-          t.tansferTpl();
         }
       },
     }
