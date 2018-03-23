ea0c43aabf8addb0abff185ad53bde759689ac68
minjie.wu <minjie.wu@chinaredstar.com>
2018-03-23 12:54:18 +0800

    修复表单回填值不正确

--- src/buildingblock/bb-form.vue
+++ src/buildingblock/bb-form.vue
@@ -69,7 +69,7 @@
             const groups = {};
             //没有group分组的项
             const normalItems = [];
-            t.newFormData = _TY_Tool.deepClone(t.contentItem);
+            t.newFormData = _TY_Tool.deepClone(t.formData);
             bbContent.forEach((field,key)=>{
                 var ref = 'form-item_' + field['uuid']
                 field['rules'] = typeof field['attributes']['rules'] == 'string'?eval(field['attributes']['rules']):field['attributes']['rules'];
