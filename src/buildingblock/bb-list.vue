3e7b80b3b96dcc3005752e482d0eac43c99ad708
minjie.wu <minjie.wu@chinaredstar.com>
2018-03-26 18:59:48 +0800

    还原列表编辑器 (去除编辑状态值自动回填)

--- src/buildingblock/bb-list.vue
+++ src/buildingblock/bb-list.vue
@@ -105,7 +105,7 @@
                         </div>
                         <div v-else>
                             <!-- 编辑状态 -->
-                            <bb v-if="scope['$index'] == canEditRow" v-mdoel="scope['row'][column.prop]" :key="scope['column']['id']" :config="column['etProp']" :alias="column['et']" :on="column['on']"></bb>
+                            <bb v-if="scope['$index'] == canEditRow" :key="scope['column']['id']" :config="column['etProp']" :alias="column['et']" :on="column['on']"></bb>
                             <!-- 只读状态 -->
                             <span v-if="scope['$index'] != canEditRow">
                                 {{scope['row'][column.prop]}}
@@ -617,6 +617,7 @@
             },
             //编辑状态，更新数据
             cellChange:function(column,val){
+                debugger
                 const t = this;
                 if(t.scope){
                     //修改
