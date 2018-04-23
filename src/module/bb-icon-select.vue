cefa3dee49e92dd841fb69905bce2ee53a7be555
minjie.wu <minjie.wu@chinaredstar.com>
2018-04-23 17:37:01 +0800

    修复积木选择器弹窗层级问题

--- src/module/bb-icon-select.vue
+++ src/module/bb-icon-select.vue
@@ -3,7 +3,9 @@
         <el-input v-model="p_value" placeholder="请选择" :disabled="true" class="wa"></el-input>
         <el-button @click="choose"><i class="el-icon-search"></i></el-button>
         <el-button @click="p_value=''" class="ml0"><i class="el-icon-delete"></i></el-button>
-
+        <bb-dialog :isShow.sync="popupVisible">
+            <bb-icon :ds="ds" ref="icon" @icon-click="iconClick"></bb-icon>
+        </bb-dialog>
     </div>
 </template>
 
@@ -23,7 +25,8 @@
         },
         data() {
             return {
-                p_value: this.value
+                p_value: this.value,
+                popupVisible:false
             }
         },
         watch: {
@@ -40,44 +43,51 @@
         methods: {
             choose(){
                 const t = this;
-                require.ensure(["art-dialog"],function(require){
-                    var _form = new Vue({
-                        router: t.$router,
-                        render: function(createElement){
-                            return createElement('bb-icon',{
-                                props:{
-                                    ds:t.ds
-                                },
-                                on:{
-                                    "icon-click": function(data){
-                                        t.p_value = data;
-                                        t.$emit('input', data);
-                                        t.dialog.close().remove();
-                                        t.dialog = null;
-                                    }
-                                },
-                                ref:"icon"
-                            },[]);
-                        }
-                    }).$mount();
+                t.popupVisible = true;
+                // require.ensure(["art-dialog"],function(require){
+                //     var _form = new Vue({
+                //         router: t.$router,
+                //         render: function(createElement){
+                //             return createElement('bb-icon',{
+                //                 props:{
+                //                     ds:t.ds
+                //                 },
+                //                 on:{
+                //                     "icon-click": function(data){
+                //                         t.p_value = data;
+                //                         t.$emit('input', data);
+                //                         t.dialog.close().remove();
+                //                         t.dialog = null;
+                //                     }
+                //                 },
+                //                 ref:"icon"
+                //             },[]);
+                //         }
+                //     }).$mount();
 
-                    var dialog = require('art-dialog');
-                    var d = dialog({
-                        width:800,
-                        // zIndex:2100,
-                        // height:'100%',
-                        title: '选择图标',
-                        content: _form.$el,
-                        onclose: function() {
-                            if (t.dialog) {
-                                t.dialog.close().remove();
-                                t.dialog = null;
-                            }
-                        }
-                    });
-                    d.showModal();
-                    t.dialog = d;
-                },'art-dialog');
+                //     var dialog = require('art-dialog');
+                //     var d = dialog({
+                //         width:800,
+                //         // zIndex:2100,
+                //         // height:'100%',
+                //         title: '选择图标',
+                //         content: _form.$el,
+                //         onclose: function() {
+                //             if (t.dialog) {
+                //                 t.dialog.close().remove();
+                //                 t.dialog = null;
+                //             }
+                //         }
+                //     });
+                //     d.showModal();
+                //     t.dialog = d;
+                // },'art-dialog');
+            },
+            iconClick:function(data){
+                const t = this;
+                t.p_value = data;
+                t.$emit('input', data);
+                t.popupVisible = false;
             }
         }
     }
