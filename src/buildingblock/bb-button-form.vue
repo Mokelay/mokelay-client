50dd65e955d7ad7177f51f04050a1b4f7a23eafe
minjie.wu <minjie.wu@chinaredstar.com>
2018-04-28 14:38:51 +0800

    解决弹窗表单值不回填bug

--- src/buildingblock/bb-button-form.vue
+++ src/buildingblock/bb-button-form.vue
@@ -149,7 +149,7 @@ import Util from '../libs/util';
                     ref:"form",
                     key:formKey
                 },[]);
-                const dialog = createElement('bb-dialog',{props:{isShow:t.formVisible,size:"middle"},on:{'update:isShow':(isShow)=>{t.formVisible = isShow;t.formData = {}}}},[form]);
+                const dialog = createElement('bb-dialog',{props:{isShow:t.formVisible,size:"middle"},on:{'update:isShow':(isShow)=>{t.formVisible = isShow;}}},[form]);
                 return dialog;
             },
             setting:function(){
