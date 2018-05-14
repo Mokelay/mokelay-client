7726f39aae4558f14f8c21f0948ae48773ac7472
陈征 <carl.chen@chinaredstar.com>
2018-05-14 20:57:21 +0800

    Vant的button实现

---
+++ src/buildingblock/vant/bb-vant-button.vue
@@ -0,0 +1,25 @@
+<template>
+  <van-button type="primary">默认按钮</van-button>
+</template>
+
+<script>
+import Button from 'vant/lib/button';
+import 'vant/lib/button/style';
+
+    export default {
+        name: 'bb-vant-button',
+        components: {
+          "van-button":Button
+        },
+        props: {},
+        data() {
+            return {
+            };
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+</style>
--- src/buildingblock/vant/index.js
+++ src/buildingblock/vant/index.js
@@ -1,9 +1,11 @@
 //Vant积木
 import vantAddressEdit from './bb-vant-address-edit';
+import vantButton from './bb-vant-button';
 
 export default {
 	install: function(Vue) {
 		Vue.component('bb-vant-address-edit', vantAddressEdit);
+		Vue.component('bb-vant-button', vantButton);
 
 		console.log("成功加载Vant积木;");
 	}
