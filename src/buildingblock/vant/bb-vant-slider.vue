9bf25c4e743404231fefe20bf3808b22e7e3fdec
Ann <joam-007@163.com>
2018-05-24 07:24:00 +0800

    增加slider组件

---
+++ src/buildingblock/vant/bb-vant-slider.vue
@@ -0,0 +1,51 @@
+<template>
+	<van-slider
+	v-model="value"
+	@change="onChange"
+	:disabled='disabled'
+	>
+	</van-slider>
+</template>
+<script>
+	import Slider from 'vant/lib/slider'
+	import 'vant/lib/slider/style';
+	// import Vue from 'vue';
+	// Vue.use(Slider);
+	export default{
+		name: "bb-vant-slider",
+		components:{
+			'van-slider':Slider
+		},
+		props:{
+		    disabled: Boolean,
+		    max: {
+		      type: Number,
+		      default: 100
+		    },
+		    min: {
+		      type: Number,
+		      default: 0
+		    },
+		    step: {
+		      type: Number,
+		      default: 1
+		    },
+		    value: {
+		      type: Number,
+		      default: 50
+		    },
+		    barHeight: {
+		      type: String,
+		      default: '2px'
+		    }
+		},
+		data(){
+			return {};
+		},
+		  methods: {
+		    onChange(value) {
+		      this.$toast('当前值：' + value);
+		    }
+		  }
+	}
+</script>
--- src/buildingblock/vant/index.js
+++ src/buildingblock/vant/index.js
@@ -1,11 +1,13 @@
 //Vant积木
 import vantAddressEdit from './bb-vant-address-edit';
 import vantButton from './bb-vant-button';
+import vantSlider from './bb-vant-slider';
 
 export default {
 	install: function(Vue) {
 		Vue.component('bb-vant-address-edit', vantAddressEdit);
 		Vue.component('bb-vant-button', vantButton);
+		Vue.component('bb-vant-slider', vantSlider);
 
 		console.log("成功加载Vant积木;");
 	}
