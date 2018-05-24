ef85305bd84c6167b9eba15bfd52f0ebcb0fdfc0
minjie.wu <minjie.wu@chinaredstar.com>
2018-05-24 15:01:21 +0800

    初始化want-cell

---
+++ src/buildingblock/vant/bb-vant-cell-group.vue
@@ -0,0 +1,54 @@
+<template>
+    <van-cell-group>
+        <bb-vant-cell v-for="(field,key) in valueBase" :key="key" :value="field.value" :option="field.option"></bb-vant-cell>
+    </van-cell-group>
+</template>
+
+<script>
+import Cell from 'vant/lib/cell';
+import 'vant/lib/cell/style';
+
+    export default {
+        name: 'bb-vant-cell-group',
+        components: {
+          "van-cell":Cell,
+        },
+        props: {
+            //内容
+            value:{
+                type:[Array]
+            },
+            /*模板默认值*/
+            defaultValTpl:{
+                type:[String,Number,Boolean]
+            },
+            /*动态获取内容*/
+            ds:{
+                type:Object
+            },
+            /*其他属性配置
+                {
+                    border:Boolean  是否显示外边框
+                }
+            */
+            option:{
+                type:Object,
+
+            }
+        },
+        data() {
+            return {
+                valueBase:this.value
+            };
+        },
+        mounted(){
+
+        },
+        //事件click
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+</style>
