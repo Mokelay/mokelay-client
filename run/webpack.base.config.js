41129e4391449bdcbcf7c49b5b16ef41bb332ab6
陈征 <carl.chen@chinaredstar.com>
2018-05-24 15:49:59 +0800

    优化打包脚本

--- run/webpack.base.config.js
+++ run/webpack.base.config.js
@@ -4,7 +4,8 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: './run/src/main'
+        main: './run/src/main',
+        h5: './run/src/h5'
     },
     output: {
         path: path.join(__dirname, './run_dist')
