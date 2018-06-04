a1573fce33a48c3530aaa8817a95cae69816335b
陈征 <carl.chen@chinaredstar.com>
2018-06-04 18:14:54 +0800

    CT增加NOT LIKE

--- src/module/bb-apilego-config.vue
+++ src/module/bb-apilego-config.vue
@@ -788,6 +788,9 @@
                                                                 {
                                                                     "text": "模糊搜索",
                                                                     "value": "like"
+                                                                },{
+                                                                    "text": "NOT LIKE",
+                                                                    "value": "notLike"
                                                                 },
                                                                 {
                                                                     "text": "左链接搜索",
