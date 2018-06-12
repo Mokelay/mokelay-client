36d7a58385ca80417a96d9d78b7476f9996b2cc8
肖兵成 <527710279@qq.com>
2018-06-12 11:35:04 +0800

    媒体库组件 #73934

--- src/buildingblock/elementui/index.js
+++ src/buildingblock/elementui/index.js
@@ -45,6 +45,7 @@ import bbformitem from './bb-form-item.vue';
 import bblist from './bb-list';
 import bbviewgroup from './bb-view-group.vue';
 import bbview from './bb-view.vue';
+import bbmedia from './bb-media.vue';
 
 import ElTreeGrid from 'element-tree-grid';
 
@@ -90,11 +91,12 @@ export default {
 		Vue.component('bb-editor-object', bbeditorobject);
 		Vue.component('bb-pop-select', bbpopselect);
 		Vue.component('bb-list', bblist);
-		Vue.component('bb-dialog', bbdialog);		
+		Vue.component('bb-dialog', bbdialog);
 		Vue.component('bb-form', bbform);
 		Vue.component('bb-bar', bbbar);
 		Vue.component('bb-form-item', bbformitem);
 		Vue.component('bb-view', bbview);
+		Vue.component('bb-media', bbmedia);
 		Vue.component('bb-view-group', bbviewgroup);
 
 		Vue.component(ElTreeGrid.name, ElTreeGrid);
