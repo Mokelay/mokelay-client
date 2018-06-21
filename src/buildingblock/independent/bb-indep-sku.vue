583905d6f2b13a1fdd968b47c085a25ed81f13da
肖兵成 <527710279@qq.com>
2018-06-21 09:33:08 +0800

    sku组件优化

--- src/buildingblock/independent/bb-indep-sku.vue
+++ src/buildingblock/independent/bb-indep-sku.vue
@@ -6,49 +6,45 @@
                 <img :src="picList[activePicIndex]">
             </div>
             <ul class="">
-                <li v-for="(item,index) in picList" @click="picClick(item,index)">
+                <li v-for="(item,index) in picList" @click="picClick(item,index)" :class="activePicIndex==index?'active':''">
                     <img :src="item"/>
                 </li>
             </ul>
         </div>
         <div class="info-box flex1">
             <dl class="title-box">
-                <dt>智能家居类A型</dt>
-                <dd>此模板是一款专门针对智能硬件的，基于机智云SDK的适用于智能空气净化器的模版。它涵盖了扫码绑定设备、WiFi绑定设备、设备控制等IOT类APP的基本功能，同时支持搜索附近设备、自定义设备属性，模板操作灵活，也适用于定制更多其他类型智能硬件产品</dd>
+                <dt>{{title}}</dt>
+                <dd>{{description}}</dd>
             </dl>
-            <dl class="info-type">
+            <dl class="info-type" v-if="tags&&tags.length>0">
                 <dt>适用对象</dt>
-                <dd>空气净化器</dd>
+                <dd v-for="(item,index) in types">{{item.text}}</dd>
             </dl>
             <ul class="btn-type">
-                <li>
-                    超值套餐
-                    <i class="iconfont ty-icon_chenggong_lv"></i>
-                </li>
-                <li>
-                    超值套餐2
+                <li v-for="(item,index) in types" @click="typeClick(item,index)" :class="activeTypeIndex==index?'active':''">
+                    {{item.text}}
                     <i class="iconfont ty-icon_chenggong_lv"></i>
                 </li>
             </ul>
             <div class="info-p">
-                <dl class="price-o">
+                <dl class="price-o" v-if="discount!=10">
                     <dt>原价</dt>
                     <dd>
                        <em>￥</em>
-                       <span class="num-o">19999</span> 
+                       <span class="num-o">{{originPrice}}</span> 
                     </dd>
                 </dl>
                 <dl class="price">
-                    <dt>折扣价</dt>
+                    <dt>{{discount!=10?'折扣价':'套餐价'}}</dt>
                     <dd>
                         <em>￥</em>
-                        <span class="num">17999</span>
-                        <sup>9折</sup>
+                        <span class="num">{{price}}</span>
+                        <sup v-if="discount!=10">{{discount}}折</sup>
                     </dd>
                 </dl>
-                <dl class="info-sm">
+                <dl class="info-sm" v-if="types[activeTypeIndex].description">
                     <dt>套餐包括:</dt>
-                    <dd>APP前端+后端+管理后台+云服务器+APP个性化调整</dd>
+                    <dd>{{types[activeTypeIndex].description}}</dd>
                 </dl>
             </div>
             <div class="btn-box">
@@ -57,8 +53,8 @@
                     <a href="javascript:;" @click="amount('add')" class="add">+</a>
                     <a href="javascript:;" @click="amount('reduce')" class="reduce">-</a>
                 </div>
-                <bb-indep-button :button="{}" class="sku-btn join" ref="join">加入我的需求</bb-indep-button>
-                <bb-indep-button :button="{}" class="sku-btn trial" ref="trial">试用体验</bb-indep-button>
+                <bb-indep-button :button="Object.assign({'selectText':'加入我的需求'},joinButton)" @click="joinClick" class="sku-btn join" ref="join"></bb-indep-button>
+                <bb-indep-button :button="Object.assign({'selectText':'试用体验'},trialButton)" class="sku-btn trial" ref="trial"></bb-indep-button>
             </div>
         </div>
     </div>
@@ -66,37 +62,194 @@
 
 <script>
     import BBIndepButton from "./bb-indep-button"
+    
     export default {
         name: 'bb-indep-sku',
         props: {
-            
+            //加入购物车按钮配置
+            joinButton:{
+                type:Object
+            },
+            //使用体验按钮配置
+            trialButton:{
+                type:Object
+            },
+            /**
+                获取商品详情数据源
+                {
+                    picList:['https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png'],
+                    title:'智能家具类A型',
+                    description:'此模板是一款专门针对智能硬件的，基于机智云SDK的适用于智能空气净化器的模版。它涵盖了扫码绑定设备、WiFi绑定设备、设备控制等IOT类APP的基本功能，同时支持搜索附近设备、自定义设备属性，模板操作灵活，也适用于定制更多其他类型智能硬件产品',
+                    tags:[{
+                        text:'空气净化器',
+                        value:'kqjhq'
+                    }],
+                    types:[{
+                        text:'超值套餐1',
+                        value:'type1',
+                        price:'9999',
+                        description:'APP前端+后端+管理后台+云服务器+APP个性化调整'
+                    }],
+                    promotion:[{
+                        year:1,
+                        discount:10
+                    },{
+                        year:2,
+                        discount:9
+                    },{
+                        year:3,
+                        discount:7
+                    }]
+                }
+            */
+            ds:{
+                type:Object
+            }
         },
         data() {
             return {
-                activePicIndex:0,
-                picList:[
-                "https://longyanpc.mmall.com/image/app_longyan.png",
-                "https://longyanpc.mmall.com/image/app_longyan.png",
-                "https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png",
-                "https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png",
-                "https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png",
-                "https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png"],
-                year:1//默认一年
+                activePicIndex:0,//主图显示的图片
+                activeTypeIndex:0,//勾选的套餐类型
+                year:1,//默认一年
+                goodsInfo:{},//商品信息，接口返回临时存储,加入购物车时，需要将商品信息返回
+
+                title:'',//标题
+                description:'',//商品描述
+                //商品图片列表
+                picList:[],
+                /**
+                    套餐：
+                    {
+                        text:'超值套餐1',
+                        value:'type1',
+                        price:'9999',
+                        description:'APP前端+后端+管理后台+云服务器+APP个性化调整'
+                    }
+                */
+                types:[],
+                /**
+                    标签
+                    {
+                        text:'空气净化器',
+                        value:'kqjhq'
+                    }
+                */
+                tags:[],
+                /**促销
+                    {
+                        year:1,
+                        discount:10
+                    }
+                */
+                promotion:[],
+                broadcastClock:null//轮播定时器
             }
         },
         components:{
             'bb-indep-button':BBIndepButton
         },
         computed:{
-            
+            //折扣
+            discount(){
+                let t=this;
+                if(!t.promotion||t.promotion.length<=0){
+                    return 10;
+                }
+                let result = 10;
+                t.promotion.forEach((item,index)=>{
+                    if(t.year == item.year){
+                        result = item.discount;
+                        return false;
+                    }
+                });
+                return result;
+            },
+            //原价
+            originPrice(){
+                let t=this;
+                let result = 0;
+                t.types.forEach((item,index)=>{
+                    if(index == t.activeTypeIndex){
+                        result = item.price;
+                        return false;
+                    }
+                });
+                return result*t.year;
+            },
+            //打折价
+            price(){
+                let t=this;
+                return Math.floor(t.originPrice * t.discount / 10);
+            }
         },
         created:function(){
             let t=this;
+            if(!t.ds){
+                //如果没有配置ds，就用测试数据显示
+                t.testData();
+            }
+            //轮播图定时器
+            t.picBroadCast();
         },
         mounted:function(){
             let t =this;
         },
+        beforeDestroy:function(){
+            let t=this;
+            //清除定时器
+            if(t.broadcastClock){
+                window.clearInterval(t.broadcastClock);
+            }
+        },
         methods: {
+            //测试数据
+            testData:function(){
+                let t=this;
+                t.title="智能家具类A型";
+                t.description="此模板是一款专门针对智能硬件的，基于机智云SDK的适用于智能空气净化器的模版。它涵盖了扫码绑定设备、WiFi绑定设备、设备控制等IOT类APP的基本功能，同时支持搜索附近设备、自定义设备属性，模板操作灵活，也适用于定制更多其他类型智能硬件产品";
+                t.picList=[
+                    "https://longyanpc.mmall.com/image/app_longyan.png",
+                    "https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png",
+                    "https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png",
+                    "https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png",
+                    "https://img3.mklimg.com/g2/M00/2D/0E/rBBrCVqWaDyAKLowAAAKmJyWFcY209.png"
+                ];
+                t.types=[{
+                    text:'超值套餐1',
+                    value:'type1',
+                    price:9999,
+                    description:'APP前端+后端+管理后台+云服务器+APP个性化调整'
+                },{
+                    text:'超值套餐2',
+                    value:'type2',
+                    price:999,
+                    description:'APP前端+后端+管理后台+云服务器+APP个性化调整2'
+                }];
+                t.tags=[{
+                    text:'空气净化器',
+                    value:'kqjhq'
+                }];
+                //促销
+                t.promotion=[{
+                    year:1,
+                    discount:10
+                },{
+                    year:2,
+                    discount:9
+                },{
+                    year:3,
+                    discount:7
+                }];
+            },
+            //图片轮播
+            picBroadCast:function(){
+                let t=this;
+                t.broadcastClock = window.setInterval(()=>{
+                    if(t.picList){
+                        t.activePicIndex = t.activePicIndex>=(t.picList.length-1)?0:(t.activePicIndex+1);
+                    }
+                },5000);
+            },
             loadChildBB(){
                 let t=this;
                 return _TY_Tool.loadChildBB(t);                
@@ -105,6 +258,24 @@
             picClick:function(item,index){
                 let t=this;
                 t.activePicIndex = index;
+            },
+            //套餐类型点击事件
+            typeClick:function(item,index){
+                let t=this;
+                t.activeTypeIndex = index;
+            },
+            //构建 sku 数据（用户选择的数据）
+            buildSkuData:function(){
+                let t=this;
+                return {
+                    type:t.types[t.activeTypeIndex],
+                    
+                }
+            },
+            //加入我的需求click事件
+            joinClick:function(button){
+                let t=this;
+
             },
             //计算年份
             amount:function(type){
@@ -123,12 +294,13 @@
         font-size: .464rem;
         .pic-box{
             width:50%;
-            margin-right:0.64rem;
+            margin-right:1.64rem;
             .pic{
                 height:14.285rem;
                 position:relative;
                 text-align: center;
                 overflow:hidden;
+                border: 0.0357rem solid #eee;
                 img{
                     width:100%;
                 } 
@@ -142,6 +314,7 @@
                     display:none;
                 }
                 li{
+                    cursor:pointer;
                     list-style:none;
                     display:inline-block;
                     width:22%;
@@ -156,6 +329,11 @@
                     }
                     &:last-child{
                         margin-right:0;
+                    }
+                    &.active{
+                        position:relative;
+                        top:-0.2857rem;
+                        border-color:#8c5ac0;
                     }
                 }
             }
