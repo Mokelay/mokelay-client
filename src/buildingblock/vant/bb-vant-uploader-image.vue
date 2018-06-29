4873cb65fff31ddcd76da8daff736e8b4c593be6
minjie.wu <minjie.wu@chinaredstar.com>
2018-06-29 20:18:30 +0800

    优化微信sdk加载时机

--- src/buildingblock/vant/bb-vant-uploader-image.vue
+++ src/buildingblock/vant/bb-vant-uploader-image.vue
@@ -150,7 +150,12 @@ export default {
                 apiUrl = window._TY_ContentPath + "/" + api;
             }
             t.uploadUrl = apiUrl;
-        }
+        };
+        _TY_Tool.wx("http://ty.saiyachina.com",
+            ["chooseImage","previewImage","uploadImage"]
+            ).then((wx)=>{
+            t.wx = wx;
+        });
     },
     render: function(createElement){
         const t = this;
@@ -254,26 +259,27 @@ export default {
             t.$emit('change',t.valueBase);
         },
         wxChooseImage(){
-            _TY_Tool.wx("http://ty.saiyachina.com",
-                ["chooseImage","previewImage","uploadImage"]
-                ).then((wx)=>{
-                wx.chooseImage({
-                    count: 1, // 默认9
-                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-                    success: function (res) {
-                        debugger
-                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                        wx.uploadImage({
-                            localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
-                            isShowProgressTips: 1, // 默认为1，显示进度提示
-                            success: function (res) {
-                                var serverId = res.serverId; // 返回图片的服务器端ID
-                                t.uploadOss(serverId);
-                            }
-                        });
-                    }
-                });
+            const t = this;
+            debugger
+            t.wx.chooseImage({
+                count: 1, // 默认9
+                sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                success: function (res) {
+                    debugger
+                    var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    wx.uploadImage({
+                        localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
+                        isShowProgressTips: 1, // 默认为1，显示进度提示
+                        success: function (res) {
+                            var serverId = res.serverId; // 返回图片的服务器端ID
+                            t.uploadOss(serverId);
+                        }
+                    });
+                },
+                error:function(res) {
+                    console.log("res:", res);
+                }
             });
         },
         //上传到oss  tx_id腾讯服务器上文件的id
