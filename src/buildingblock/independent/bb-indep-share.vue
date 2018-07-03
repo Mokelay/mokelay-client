7588046b6eade61c0dc2335bd28973ab6e982f00
minjie.wu <minjie.wu@chinaredstar.com>
2018-07-03 11:36:33 +0800

    注册并实现分享组件

---
+++ src/buildingblock/independent/bb-indep-share.vue
@@ -0,0 +1,122 @@
+<script>  
+export default {
+    name:"bb-indep-share",
+    components:{
+    },
+    props:{
+        //分享标题
+        title:{
+            type:String
+        },
+        // 分享描述
+        desc: {
+            type:String
+        },
+        // 分享链接
+        link: {
+            type:String
+        },
+        // 分享图标
+        imgUrl: {
+            type:String
+        },
+        // 分享类型,music、video或link，不填默认为link
+        type: {
+            type:String
+        },
+        // 如果type是music或video，则要提供数据链接，默认为空
+        dataUrl: {
+            type:String
+        },
+        /*其他属性设置 
+            {
+                icon：""  触发分享的图标
+                shareArea:"timeline,appMessage,QQ,weibo,qZone"  允许分享范围  默认全部允许
+            }
+        */
+        option:{
+            type:Object,
+            default:function(){
+                return {
+                    icon:"ty-riji-fenxiang",
+                    shareArea:"timeline,appMessage,QQ,weibo,qZone"
+                }
+            }
+        }
+    },
+   data(){ 
+        return{
+            //分享内容
+            shareInfo:{},
+            realTitle:this.title,
+            realDesc:this.desc,
+            realLink:this.link,
+            realImgUrl:this.imgUrl,
+            shareArea:this.option.shareArea.split(",")
+        };
+    },
+    mounted: function () {
+        const t = this;
+        _TY_Tool.wx(
+            ['onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone']
+            ).then((wx)=>{
+                t.wx = wx;
+            });
+        t.realTitle = _TY_Tool.tpl(t.title,_TY_Tool.buildTplParams(t));
+        t.realDesc = _TY_Tool.tpl(t.desc,_TY_Tool.buildTplParams(t));
+        t.realLink = _TY_Tool.tpl(t.link,_TY_Tool.buildTplParams(t));
+        t.realImgUrl = _TY_Tool.tpl(t.imgUrl,_TY_Tool.buildTplParams(t));
+    },
+    render: function(createElement){
+        const t = this;
+        const shareButton = createElement('i',{class:`ty ${t.option.icon}`,on:{click:t.startShare}},[]);
+        return shareButton;
+    },
+    watch:{
+
+    },  
+    methods: {
+        //获取子积木
+        loadChildBB(){
+            let t=this;
+            return _TY_Tool.loadChildBB(t);                
+        },
+        startShare(){
+            const t = this;
+            const shareInfo = {
+                title:t.realTitle,
+                desc:t.realDesc,
+                link:t.realLink,
+                imgUrl:t.realImgUrl,
+            };
+            if(t.wx){
+                t.shareArea.forEach((val,key)=>{
+                    switch(val){
+                        case "timeline":
+                            t.wx.onMenuShareTimeline(shareInfo);
+                            break;
+                        case "appMessage":
+                            t.wx.onMenuShareAppMessage(shareInfo);
+                            break;
+                        case "QQ":
+                            t.wx.onMenuShareQQ(shareInfo);
+                            break;
+                        case "weibo":
+                            t.wx.onMenuShareWeibo(shareInfo);
+                            break;
+                        case "qZone":
+                            t.wx.onMenuShareQZone(shareInfo);
+                            break;
+                    }
+                })
+            }else{
+                return;
+            }
+        }
+    }
+}
+</script>
+<style lang="less">
+
+</style>
+
--- src/buildingblock/independent/index.js
+++ src/buildingblock/independent/index.js
@@ -42,6 +42,7 @@ import bbindepuploaderaudio from './bb-indep-uploader-audio';
 import bbindepaudioplay from './bb-indep-audio-player';
 import bbindepBlogList from './bb-indep-blog-list';
 import bbh5config from './bb-h5-config';
+import bbindepshare from './bb-indep-share';
 
 import helloworld from './bb-hello-world';
 
@@ -93,6 +94,7 @@ export default {
 		Vue.component('bb-indep-audio-player', bbindepaudioplay);
 		Vue.component('bb-indep-blog-List', bbindepBlogList);
 		Vue.component('bb-h5-config', bbh5config);
+		Vue.component('bb-indep-share', bbindepshare);
 
 		Vue.component('bb-hello-world', helloworld);
 
