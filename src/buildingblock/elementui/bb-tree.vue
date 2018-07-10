710338e644f9ee9e2c905d2c13edcbd08b676d67
minjie.wu <minjie.wu@chinaredstar.com>
2018-07-10 11:23:22 +0800

    增加bb-tree默认值 方便配置

--- src/buildingblock/elementui/bb-tree.vue
+++ src/buildingblock/elementui/bb-tree.vue
@@ -75,6 +75,40 @@
             staticDs:{
                 type:Object
             },
+            //一次性获取全部静态数据
+            staticData:{
+                type:Array,
+                default:function(){
+                    return [{
+                          id: 1,
+                          label: '一级 2',
+                          children: [{
+                            id: 3,
+                            label: '二级 2-1',
+                            children: [{
+                              id: 4,
+                              label: '三级 3-1-1'
+                            }, {
+                              id: 5,
+                              label: '三级 3-1-2',
+                              disabled: true
+                            }]
+                          }, {
+                            id: 2,
+                            label: '二级 2-2',
+                            disabled: true,
+                            children: [{
+                              id: 6,
+                              label: '三级 3-2-1'
+                            }, {
+                              id: 7,
+                              label: '三级 3-2-2',
+                              disabled: true
+                            }]
+                          }]
+                        }]
+                }
+            },
             external: {
                 type: Object
             },
@@ -95,7 +129,7 @@
         data() {
             const t = this;
             return {
-                data: [],
+                data: t.staticData,
                 id:"bb-tree_" + _TY_Tool.uuid() 
             }
         },
