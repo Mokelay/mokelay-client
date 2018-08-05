198e3fe2083d8838c8dd7065cad9f8d507defcd7
陈征 <carl.chen@chinaredstar.com>
2018-08-05 21:55:08 +0800

    默认配置改为Mokelay，如果要调试红星内网业务，可以单独修改env，但是代码不要提交上来

--- run/src/env/env.js
+++ run/src/env/env.js
@@ -6,14 +6,18 @@ if (window) {
 var env = {
 	local: {
 		name: 'local',
-		apiHost: "http://longyan.dev.rs.com",
+		apiHost: "http://www.mokelay.com",
+		// apiHost: "http://longyan.dev.rs.com",
 		h5Host: CurrentDomain,
 		rsURL: "http://urms.dev.rs.com/passport/login?service=http://longyan.dev.rs.com/urmscallback",
 		ssoURL: CurrentDomain + "/#/ty-login",
 		hosts: {
-			'default': "http://longyan.dev.rs.com"
+			// 'default': "http://longyan.dev.rs.com"
+			'default': "http://www.mokelay.com"
 		},
-		questionnaireHost: "http://ty.saiyachina.com/survey"
+		// questionnaireHost: "http://ty.saiyachina.com/survey"
+		questionnaireHost: "http://www.mokelay.com/survey",
+		home: "/home_ln"
 	},
 	prd: {
 		name: 'prd',
