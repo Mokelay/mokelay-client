6adfbe678270c1d6ba48df25f6712b371e948b00
minjie.wu <minjie.wu@chinaredstar.com>
2018-08-24 17:30:18 +0800

    优化pdf积木

--- src/buildingblock/vendor/bb-pdf/bb-pdf.vue
+++ src/buildingblock/vendor/bb-pdf/bb-pdf.vue
@@ -1,7 +1,13 @@
 <template>
-    <bb-indep-dialog :isShow="showDialog" :closeOnClickOverlay="true">
-        <div ref="pdf" id="pdf">
+    <bb-indep-dialog class="bb-pdf" :isShow="showDialog" :closeOnClickOverlay="true" :showConfirmButton="false">
+        <div v-if="loading" class="spinner">
+            <div class="rect1"></div>
+            <div class="rect2"></div>
+            <div class="rect3"></div>
+            <div class="rect4"></div>
+            <div class="rect5"></div>
         </div>
+        <div ref="pdf" id="pdf"></div>
     </bb-indep-dialog>
 </template>
 <script>
@@ -12,7 +18,7 @@ export default {
         value: {
             //pdf路径  支持模板
             type: String,
-            default: ''
+            default: 'http://cdn.mozilla.net/pdfjs/tracemonkey.pdf'
         },
         lazy: {
             type: Boolean,
@@ -22,7 +28,10 @@ export default {
     data () {
         return {
             valueBase:this.value,
-            showDialog:false
+            showDialog:false,
+            currentPage: 1,
+            pageCount: 1,
+            loading:false
         };
     },
     created: function () {
@@ -39,29 +48,121 @@ export default {
             let t=this;
             //引入第三方vant 的dialog组件
             t.showDialog = true;
+            t.loading = true;
             setTimeout(()=>{
-                const pdf = r => require.ensure([], () => r(require('vue-pdf')), 'vue-pdf');
+                const pdf = r => require.ensure([], () => r(require('vue-pdf')), 'vue-pdf').defalut;
+                // var loadingTask = pdf.createLoadingTask(url);
                 let Vue = require('vue').default;
                 var vm = new Vue({
                     render:function (createElement) {
-                        const t = this;
-                        return createElement("pdf",{props:{src:url}});
+                        const preButton = createElement("button",{props:{src:url,page:t.currentPage},on:{
+                                "click":t.prePage
+                            }},"上一页");
+                        const pageNumber = createElement("span",{},[`${t.currentPage}/${t.pageCount}`]);
+                        const nextButton = createElement("button",{props:{src:url,page:t.currentPage},on:{
+                                "click":t.nextPage
+                            }},"下一页");
+                        const buttons = createElement("span",{class:"buttons"},[preButton,pageNumber,nextButton]);
+                        const pdfElement = createElement("pdf",{props:{src:url,page:t.currentPage},on:{
+                                "num-pages":($event)=>{t.pageCount = $event?$event:t.pageCount;},
+                                "page-loaded":($event)=>{t.currentPage = $event;t.loading = false;}
+                            }});
+                        return createElement("div",{class:"pdf"},[pdfElement,buttons])
                     },
                     components: {
                         pdf
                     },
                     data() {
                         return {
-                            currentPage: 0,
-                            pageCount: 0,
                         }
+                    },
+                    mounted() {
+                    },
+                    methods:{
                     }
                 }).$mount('#pdf');
             },0);
+        },
+        prePage(){
+            const t = this;
+            if(t.currentPage > 1){
+                t.currentPage--;
+            }
+        },
+        nextPage(){
+            const t = this;
+            if(t.currentPage < t.pageCount){
+                t.currentPage++;
+            }
         }
     }
 }
 </script>
  
-<style scoped>
+<style lang="less">
+    .bb-pdf{
+        .pdf{
+            max-height:90vh;
+            overflow: auto;
+        }
+        .buttons{
+            text-align:center;
+            display:flex;
+            &>span,&>button{
+                flex:1;
+            }
+        }
+        .spinner {
+          margin: 100px auto;
+          width: 50px;
+          height: 60px;
+          text-align: center;
+          font-size: 10px;
+        }
+         
+        .spinner > div {
+          background-color: #67CF22;
+          height: 100%;
+          width: 6px;
+          display: inline-block;
+           
+          -webkit-animation: stretchdelay 1.2s infinite ease-in-out;
+          animation: stretchdelay 1.2s infinite ease-in-out;
+        }
+         
+        .spinner .rect2 {
+          -webkit-animation-delay: -1.1s;
+          animation-delay: -1.1s;
+        }
+         
+        .spinner .rect3 {
+          -webkit-animation-delay: -1.0s;
+          animation-delay: -1.0s;
+        }
+         
+        .spinner .rect4 {
+          -webkit-animation-delay: -0.9s;
+          animation-delay: -0.9s;
+        }
+         
+        .spinner .rect5 {
+          -webkit-animation-delay: -0.8s;
+          animation-delay: -0.8s;
+        }
+         
+        @-webkit-keyframes stretchdelay {
+          0%, 40%, 100% { -webkit-transform: scaleY(0.4) } 
+          20% { -webkit-transform: scaleY(1.0) }
+        }
+         
+        @keyframes stretchdelay {
+          0%, 40%, 100% {
+            transform: scaleY(0.4);
+            -webkit-transform: scaleY(0.4);
+          }  20% {
+            transform: scaleY(1.0);
+            -webkit-transform: scaleY(1.0);
+          }
+        }
+    }
 </style>
