dc267bf1622cabb7edc9575b6ca75677748b7101
xiaobc <527710279@qq.com>
2018-09-03 09:56:32 +0800

    优化配置页rem

--- src/libs/flexble.js
+++ src/libs/flexble.js
@@ -19,6 +19,8 @@
       docEl.style.fontSize = '28px'
     } else if (_TY_Tool && _TY_Tool.isPC()) {
       docEl.style.fontSize = '28px'
+    } else if (_TY_Root.$route.params && _TY_Root.$route.params.pageAlias == 'ty-bb-config') {
+      docEl.style.fontSize = '28px'
     } else {
       docEl.style.fontSize = rem + 'px'
     }
