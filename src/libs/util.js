d5aa9f75e00c309a87f354b8d6d2699637316da2
陈征 <carl.chen@chinaredstar.com>
2018-09-17 13:16:41 +0800

    如果编辑模式下，为登录授权的页面不做跳转，只做提示

--- src/libs/util.js
+++ src/libs/util.js
@@ -28,6 +28,10 @@ util.invoke = function(options) {
                 _clearLoading();
                 location.href = response['data']['message'] || window._TY_SSOURL;
             } else if (response && response['data'] && response['data']['code'] && response['data']['code'] <= -400) {
+                if(window._TY_BB_Edit){
+                    window._TY_Toast({content:"提示：该页面需要有登陆态情况下才会展示正确数据"});
+                    return;
+                }
                 //所有Code小于等于-400都是属于没有登录授权的，统一走SSOURL配置路径
                 if (response['data']['code'] == -401) {
                     _clearLoading();
