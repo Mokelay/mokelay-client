3154506b9cf7ed1f5ac71d2f5564b9c45dedfedf
xiaobc <527710279@qq.com>
2018-11-08 09:58:13 +0800

    解决  bb-indep-ul 组件内部配置按钮弹窗后，弹窗获取不到rowData值的问题（新增bb-div组件，可以包装所有需要传值的组件，可自定义标签名）

---
+++ src/buildingblock/independent/bb-div.vue
@@ -0,0 +1,48 @@
+<script>
+  export default {
+    name: 'bb-div',
+    render: function (createElement) {
+        const t = this;
+        return createElement(t.tag,{
+                class:t.clazz,
+                style:t.cssStyle,
+                on:{
+                    click:function(){
+                        t.$emit('click');
+                    }
+                }
+            },_TY_Tool.bbRender(t.content,createElement,t));
+    },
+    props: {
+    	tag:{
+    		type:String,
+    		default:'div'
+    	},
+    	content:{
+    		type:Array
+    	},
+       	clazz:{
+       		type:[Array,String]
+       	},
+       	cssStyle:{
+       		type:Object
+       	},
+        params:{
+          type:Object
+        }
+    },
+    data() {
+      return {}
+    },
+    computed:{
+        
+    },
+    created: function () {
+    },
+    methods: {
+      
+    }
+  }
+</script>
+<style>
+</style>
